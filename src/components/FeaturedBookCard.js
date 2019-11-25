2efe69a320a412cdeb405800286a4f166888d8c0
William Blacknall <williamblacknall@gmail.com>
2019-11-25 15:38:45 -0500

    Added styling to GenreCard

---
+++ src/components/FeaturedBookCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./components.css"
+
+const FeaturedBookCard = props => {
+    return (<div className="featuredBookCard">
+        <h1
+    </div>)
+}
--- src/stories/3-GenreCard.stories.js
+++ src/stories/3-GenreCard.stories.js
@@ -1,7 +1,12 @@
 import React from "react";
-import {storiesOf} from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 
-import GenreCard from "../components/GenreCard"
+import GenreCard from "../components/GenreCard";
 
-storiesOf("GenreCard", module)
-.add("Book Genres", () => <GenreCard title="GoobleGobble" bookTitle="Tales of GibblyGoo" text="Enter the tales of gibbledee gee!" />)
+storiesOf("GenreCard", module).add("Book Genres", () => (
+	<GenreCard
+		title="eget aliquet"
+		bookTitle="ornare massa eget"
+		text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	/>
+));
